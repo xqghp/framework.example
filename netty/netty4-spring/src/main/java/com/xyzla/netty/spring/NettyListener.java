6fb0580713b8d08ddb6e897a4bb9bf34768e628f
qqghp <xqghp@126.com>
2019-07-25 19:27:33 +0800

    netty 若在tomcat下启动,则需要 new Thread(){}.start();否则会阻塞Tomcat 主线程

--- netty/netty4-spring/src/main/java/com/xyzla/netty/spring/NettyListener.java
+++ netty/netty4-spring/src/main/java/com/xyzla/netty/spring/NettyListener.java
@@ -1,22 +1,29 @@
 package com.xyzla.netty.spring;
 
-import com.sun.crypto.provider.BlowfishKeyGenerator;
 import com.xyzla.netty.spring.server.ServerManager;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+/**
+ * 必须重开一个线程运行 netty,否则将会阻塞tomcat 主线程.
+ */
 public class NettyListener implements ServletContextListener {
 
     ServerManager manager;
 
     public void contextInitialized(ServletContextEvent sce) {
-        manager = new ServerManager();
-        try {
-            manager.startServer();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        new Thread() {
+            @Override
+            public void run() {
+                manager = new ServerManager();
+                try {
+                    manager.startServer();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
